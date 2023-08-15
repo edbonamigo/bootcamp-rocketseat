659b03e146e8209161e0e0408400fc5a9305d73c
edbonamigo <edbmuller@gmail.com>
2023-08-15 19:31:01 -0300

    :sparkles: Add styles components, defaultTheme setup and type definition for styles with typescript

--- timer-ignite/src/App.tsx
+++ timer-ignite/src/App.tsx
@@ -1,8 +1,10 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/defalt";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Timer Ignite!</h1>
-    </>
+    </ThemeProvider>
   );
 }
