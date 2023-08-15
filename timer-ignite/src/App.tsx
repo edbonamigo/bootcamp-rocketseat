d1a9b258c76381dc3ba1a71cf56423ef7f1d736c
edbonamigo <edbmuller@gmail.com>
2023-08-15 20:50:45 -0300

    :lipstick: Add fonts and colors

--- timer-ignite/src/App.tsx
+++ timer-ignite/src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/defalt";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Timer Ignite!</h1>
+
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
