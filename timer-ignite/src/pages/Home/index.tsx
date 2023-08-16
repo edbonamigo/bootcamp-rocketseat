baebd455b252fcd62374d860b5b242248115dd7e
edbonamigo <edbmuller@gmail.com>
2023-08-16 16:05:47 -0300

    :lipstick: Add Input component and styles

--- timer-ignite/src/pages/Home/index.tsx
+++ timer-ignite/src/pages/Home/index.tsx
@@ -15,10 +15,27 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">I will work</label>
-          <TaskInput id="task" placeholder="Give a name for your project" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Give a name for your project"
+          />
+          <datalist id="task-suggestions">
+            <option value="Ignite"></option>
+            <option value="Website jubonamigo.com"></option>
+            <option value="Design webtvsap"></option>
+            <option value="Design Atelie Neiva"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">for</label>
-          <MinutesAmountInput id="minutesAmount" placeholder="00" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutes.</span>
         </FormContainer>
