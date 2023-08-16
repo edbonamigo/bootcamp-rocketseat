d8f2e987a724a46e609f7d43518541364d3838a2
edbonamigo <edbmuller@gmail.com>
2023-08-16 15:46:56 -0300

    :lipstick: Add Home component and styles

---
+++ timer-ignite/src/pages/Home/index.tsx
@@ -0,0 +1,39 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">I will work</label>
+          <TaskInput id="task" placeholder="Give a name for your project" />
+
+          <label htmlFor="minutesAmount">for</label>
+          <MinutesAmountInput id="minutesAmount" placeholder="00" />
+
+          <span>minutes.</span>
+        </FormContainer>
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
