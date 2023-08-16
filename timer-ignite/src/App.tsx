0a681dbe3dd92144d5284c533774d1f1d3f33b3f
edbonamigo <edbmuller@gmail.com>
2023-08-16 10:50:16 -0300

    :sparkles: Add Pages and Routes - React Router DOM

--- timer-ignite/src/App.tsx
+++ timer-ignite/src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 import { defaultTheme } from './styles/themes/defalt'
 import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Timer Ignite!</h1>
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
